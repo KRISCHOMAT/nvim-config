589470a1e4377253bbf3c2361077ba679acf47a1
Christian Grothe <christian.grothe@posteo.de>
2024-04-30 15:03:45 +0200

    V0.23

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
+vim.cmd("set number")
 vim.g.mapleader = ' '
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-L>", vim.cmd.vsplit)
